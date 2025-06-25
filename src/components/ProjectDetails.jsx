3f922ee2283e89b65bdfcbf7bbc8bb96635bfeeb
Bmoore4452 <r.moore803@gmail.com>
2025-06-25 18:06:00 -0400

    Update: fixed project detail pop up

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -9,49 +9,70 @@ const ProjectDetails = ({
   closeModal,
 }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm sm:w-auto">
-      <motion.div
-        className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-      >
-        <button
-          onClick={closeModal}
-          className="absolute p-2 rounded-sm top-5 right-5 bg-midnight hover:bg-gray-500"
-        >
-          <img src={`${import.meta.env.BASE_URL}assets/close.svg`} className="w-6 h-6" />
-        </button>
-        <img src={`${import.meta.env.BASE_URL}${image}`} alt={title} className="w-full rounded-t-2xl" />
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold text-white">{title}</h5>
-          <p className="mb-3 font-normal text-neutral-400">{description}</p>
-          {subDescription.map((subDesc, index) => (
-            <p key={index} className="mb-3 font-normal text-neutral-400">{subDesc}</p>
-          ))}
-          <div className="flex items-center justify-between mt-4">
-            <div className="flex gap-3">
-              {tags.map((tag) => (
-                <img
-                  key={tag.id}
-                  src={`${import.meta.env.BASE_URL}${tag.path}`}
-                  alt={tag.name}
-                  className="rounded-lg size-10 hover-animation"
-                />
-              ))}
-            </div>
-            <a
-              className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation"
-              href={href}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View Project{" "}
-              <img src={`${import.meta.env.BASE_URL}assets/arrow-up.svg`} className="size-4" />
-            </a>
-          </div>
-        </div>
-      </motion.div>
-    </div>
+      <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm">
+          <motion.div
+              className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+          >
+              <button
+                  onClick={closeModal}
+                  className="absolute p-2 rounded-sm top-2 right-2 sm:top-5 sm:right-5 bg-midnight hover:bg-gray-500 z-10"
+              >
+                  <img
+                      src={`${import.meta.env.BASE_URL}assets/close.svg`}
+                      className="w-6 h-6"
+                  />
+              </button>
+              <img
+                  src={`${import.meta.env.BASE_URL}${image}`}
+                  alt={title}
+                  className="w-full rounded-t-2xl"
+              />
+              <div className="p-8 max-sm:text-sm">
+                  <h5 className="mb-2 text-2xl font-bold text-white">
+                      {title}
+                  </h5>
+                  <p className="mb-3 font-normal text-neutral-400 text-wrap">
+                      {description}
+                  </p>
+                  {subDescription.map((subDesc, index) => (
+                      <p
+                          key={index}
+                          className="mb-3 font-normal text-neutral-400"
+                      >
+                          {subDesc}
+                      </p>
+                  ))}
+                  <div className="flex items-center justify-between mt-4">
+                      <div className="flex gap-3">
+                          {tags.map((tag) => (
+                              <img
+                                  key={tag.id}
+                                  src={`${import.meta.env.BASE_URL}${tag.path}`}
+                                  alt={tag.name}
+                                  className="rounded-lg size-10 hover-animation"
+                              />
+                          ))}
+                      </div>
+                      <a
+                          className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation"
+                          href={href}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          View Project{' '}
+                          <img
+                              src={`${
+                                  import.meta.env.BASE_URL
+                              }assets/arrow-up.svg`}
+                              className="size-4"
+                          />
+                      </a>
+                  </div>
+              </div>
+          </motion.div>
+      </div>
   );
 };
 
