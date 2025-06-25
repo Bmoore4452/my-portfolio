6e027486b23647a7b064d80d57f2d3fe5cf305a5
Bmoore4452 <r.moore803@gmail.com>
2025-06-25 18:23:59 -0400

    Update: fixed project detail pop up 4

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -11,7 +11,7 @@ const ProjectDetails = ({
   return (
       <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-hidden backdrop-blur-sm max-sm:scale-90 max-sm:backdrop-blur-none">
           <motion.div
-              className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
+              className="relative max-w-2xl border max-sm:max-h-fit shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
           >
